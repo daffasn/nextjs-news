22d2591e3ffbe9efcded65482254be2d1b47d83b
daffaseptian24@gmail.com <daffaseptian24@gmail.com>
2024-10-13 20:55:52 +0700

    files changed 1.2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center'>
       <div className='flex flex-col'>
-        <Link href='/' className='text-4xl font-bold'>
+        <Link href='/' className='text-3xl sm:text-4xl font-bold'>
             <span className='underline underline-offset-[3.4px]'>Next Blog</span>
             <span>.</span>
         </Link>
